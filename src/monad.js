a9b4a06dacac84ed1a03e8cf03581c77fcf095c8
Carlos Vializ <cvializ@vt.edu>
2023-09-22 09:18:40 -0400

    wow

--- src/monad.js
+++ src/monad.js
@@ -1,11 +1,46 @@
 // Implements https://github.com/fantasyland/static-land/blob/master/docs/spec.md#monad
 
+// T describes a Category? Category of zero-argument functions returning primitives.
+
 const T = (a) => () => a;
 
 const getTValue = (valueT) => valueT();
 
 
 const compose = (...fns) => fns.reduce((a, b) => (...args) => a(b(...args)));
+
+// What's the operation? wrapping in successive function calls?
+
+// const concat = (a, b) => [a, b];
+
+// (a, b, c, d) => [a, b, c, d]
+// a => b => c => d => [a, b, c, d];
+
+// (a) => (b) => [a, b];
+// () => [];
+
+// const x = of([1]);
+// const y = of([2]);
+
+// concat(x, y) === [1, 2]
+
+
+
+
+
+// const createMonoid = (value, nextValue) => (extract) => extract(value, nextValue);
+
+// const withHom = (nextValue) => (value) => createMonoid(value, createMonoid(nextValue));
+
+// const monoid = (value) => createMonoid(value, null);
+
+// const nextMonoid = withHom(6)(monoid(5))
+
+// const monoid5 = monoid(5);
+// monoid5(console.log)
+
+
+
 
 // Applicative
 
@@ -27,10 +62,9 @@ const of = (a) => T(a);
 
 const map = (predicate, aT) => T(predicate(aT()));
 
-// const test = (f, g) => map(x => f(g(x)), of(1))() === map(f, map(g, of(1)))();
-// console.log('identity', map(x => x, of(1))());
-// console.log('composition', test(x => x+1, x => x+10));
-
+const test = (f, g) => map(x => f(g(x)), of(1))() === map(f, map(g, of(1)))();
+console.log('identity', map(x => x, of(1))());
+console.log('composition', test(x => x+1, x => x+10));
 
 // Apply
 
@@ -116,32 +150,100 @@ const curryChain = predicateReturningTValue => aT => map(predicateReturningTValu
 
 
 
-const prop = p => o => o[p];
+// const prop = p => o => o[p];
 
-const getUsername = account => chain(prop('name'), chain(prop('user'), chain(prop('personal'), of(account))))();
+// const getUsername = account => chain(prop('name'), chain(prop('user'), chain(prop('personal'), of(account))))();
 
-// const getUsername2 = account => map(
-//     compose(
-//         prop('name'),
-//         prop('user'),
-//         prop('personal'),
-//     ),
-//     of(account)
-// )
+// // const getUsername2 = account => map(
+// //     compose(
+// //         prop('name'),
+// //         prop('user'),
+// //         prop('personal'),
+// //     ),
+// //     of(account)
+// // )
 
-const getUsername2 = account => compose(
-    curryChain(prop('personal')),
-    curryChain(prop('user')),
-    curryChain(prop('name')),
-)(of(account))();
+// const getUsername2 = account => compose(
+//     curryChain(prop('personal')),
+//     curryChain(prop('user')),
+//     curryChain(prop('name')),
+// )(of(account))();
 
-// Might be retrieved async!
-const user = {
-  personal: {
-    user: {
-      name: 'John Doe'
-    }
-  }
-}
+// // Might be retrieved async!
+// const user = {
+//   personal: {
+//     user: {
+//       name: 'John Doe'
+//     }
+//   }
+// }
 
-console.log(getUsername2(user));
+// console.log(getUsername2(user));
+
+
+
+
+
+// const f = x => x + 1;
+// const x = 10;
+
+// const result = ap(of(f), of(x))() === of(f(x))();
+
+// console.log(result)
+
+
+// COMPOSITION OF FSCKING VALUES MONOID OH YEAH
+
+const call = (left, right) => right(left());
+
+// const x = call(T(2), call(T(1), (a) => (b) => (fn) => fn(a, b)));
+
+// ap(x, T(console.log))
+
+// id = x=>x
+
+// unit = (value) => (fn) => fn(value)
+
+// value = unit('value')(T);
+
+// ofUnit = a => unit(a)
+
+// console.log(ofUnit('value'))
+
+
+// function curryLog (a) {
+//     console.log(a);
+//     return curryLog;
+// }
+
+// concat = (firstUnit, secondUnit) => unit(firstUnit, unit(secondUnit));
+
+// v = concat(unit(1), unit(2))
+// console.log(v(curryLog))
+
+
+// const concat = (one, two) => call(one, call(two, (a) => (b) => fn => fn(a)(b)))
+
+// u = concat(T(1), concat(T(2), T(3)))
+
+// console.log(u)
+
+
+// u(curryLog)
+
+
+
+// map(u, curryLog)
+// ap(curryLog, u)
+
+// ap(T(curryLog), u)
+
+// concat()
+
+// const value = call(T(2), call(T(1), (a) => (b) => fn => fn(a, b)));
+
+// ap(value, console.log);
+
+
+
+////////
